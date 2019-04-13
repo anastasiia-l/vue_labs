761719ea78b3896e6682b2b79b9e3743b133f56a
anastasiia-l <anastasiia.leshkevych@nure.ua>
2019-04-13 19:47:38 +0300

    Create Task 2,4

---
+++ src/components/Lab1Task4.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <b-card
+                no-body
+                style="max-width: 20rem;"
+                img-src="https://placekitten.com/380/200"
+                img-alt="Image"
+                img-top
+        >
+            <h4 slot="header">Hello World</h4>
+
+            <b-card-body>
+                <b-card-title>Card Title</b-card-title>
+            </b-card-body>
+            <li>
+
+            </li>
+            <b-list-group  v-for="item in breeds" flush>
+                <b-list-group-item>{{ item }}</b-list-group-item>
+            </b-list-group>
+        </b-card>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Lab1Task4",
+        data: function () {
+            return {
+                breeds: this.methods.uploadBreeds(),
+            }
+        },
+        methods: {
+            uploadBreeds: function () {
+                let url = 'https://dog.ceo/api/breeds/list/all';
+                let list = []
+                fetch(url) // Call the fetch function passing the url of the API as a parameter
+                    .then(function(data) {
+                        console.log(data);
+                        list = data
+                    })
+                    .catch(function() {
+                        // This is where you run code if the server returns any errors
+                    });
+                return list
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
